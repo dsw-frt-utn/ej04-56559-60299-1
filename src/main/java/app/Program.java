3949d255ffbf25993c9ad4a1b54a9e54b97a5677
Lisandro <lisandrorivassunen113@gmail.com>
2026-04-19 16:41:51 -0300

    Implementacion de ventana de Ingreso de Vehiculo y otros arreglos

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -2,12 +2,18 @@ package app;
 
 import data.Persistencia;
 import java.util.InvalidPropertiesFormatException;
-import views.ListarVehiculosView;
+import views.*;
 
 public class Program {
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
         ListarVehiculosView view = new ListarVehiculosView();
         view.setVisible(true);
+        
+        /*IngresarVehiculo v2 = new IngresarVehiculo();
+        v2.setVisible(true);*/
+        
+        
+        
     }
 }
--- src/main/java/data/Persistencia.java
+++ src/main/java/data/Persistencia.java
@@ -56,4 +56,8 @@ public class Persistencia {
         inicializarSucursales();
         inicializarVehiculos();
     }
+    
+        public static void addVehiculo(Vehiculo v){
+        vehiculos.add(v);
+    }
 }
